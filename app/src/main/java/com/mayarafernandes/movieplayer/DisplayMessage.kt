0a90a3eeae7904f85fda6881d8608111d97ff18c
Mayara Fernandes <mayara.mpf21@gmail.com>
2019-08-16 16:42:53 -0300

    Saves email after successful login

--- app/src/main/java/com/mayarafernandes/movieplayer/DisplayMessage.kt
+++ app/src/main/java/com/mayarafernandes/movieplayer/DisplayMessage.kt
@@ -1,7 +1,9 @@
 package com.mayarafernandes.movieplayer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_display_message.*
 
@@ -16,5 +18,15 @@ class DisplayMessage : AppCompatActivity() {
         val textView4 = findViewById<TextView>(R.id.textView4).apply {
             text = message
         }
+    }
+
+    fun onClickForget(view: View) {
+        val sharedPref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            remove("savedEmail")
+            apply()
+            //commit()
+        }
+
     }
 }
--- app/src/main/java/com/mayarafernandes/movieplayer/MainActivity.kt
+++ app/src/main/java/com/mayarafernandes/movieplayer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mayarafernandes.movieplayer
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.view.View
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val EXTRA_MESSAGE = "com.mayarafernandes.movieplayer.MESSAGE"
@@ -20,6 +22,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return
+        val savedEmail = sharedPref.getString("savedEmail", null)
+
+        emailEditText.setText(savedEmail, TextView.BufferType.EDITABLE)
 
         val emailBox = emailEditText
         val passwordBox = passwordEditText
@@ -45,10 +52,18 @@ class MainActivity : AppCompatActivity() {
         //val email = findViewById<EditText>(R.id.emailEditText).text.toString()
         //val password = findViewById<EditText>(R.id.passwordEditText).text.toString()
 
+
         val email = emailEditText.text.toString()
         val password = passwordEditText.text.toString()
 
         if(email == MASTER_EMAIL && password == MASTER_PASSWORD) {
+            val sharedPref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE) ?: return
+            with (sharedPref.edit()) {
+                putString("savedEmail", email)
+                apply()
+                //commit()
+            }
+
             val message = "$email logado.\nSeja bem vindo!"
 
             val intent = Intent(this, DisplayMessage::class.java). apply {
