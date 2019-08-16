c90d8f92571f6e0546de5564367d8a791386e503
Mayara Fernandes <mayara.mpf21@gmail.com>
2019-08-16 15:52:14 -0300

    Shows if credentials are incorrect.

--- app/src/main/java/com/mayarafernandes/movieplayer/MainActivity.kt
+++ app/src/main/java/com/mayarafernandes/movieplayer/MainActivity.kt
@@ -4,23 +4,49 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 const val EXTRA_MESSAGE = "com.mayarafernandes.movieplayer.MESSAGE"
+const val LOGIN_ERROR_MESSAGE = "E-mail ou senha incorretos"
 const val MASTER_EMAIL = "admin@mail.com"
 const val MASTER_PASSWORD = "admin123"
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val emailBox = emailEditText
+        val passwordBox = passwordEditText
+
+        emailBox.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable) { loginErrorMessageText.text = "" }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+
+        passwordBox.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable) { loginErrorMessageText.text = "" }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
+
+
     }
+
 
     fun onLogin(view: View) {
 
-        val email = findViewById<EditText>(R.id.emailEditText).text.toString()
-        val password = findViewById<EditText>(R.id.passwordEditText).text.toString()
+        //val email = findViewById<EditText>(R.id.emailEditText).text.toString()
+        //val password = findViewById<EditText>(R.id.passwordEditText).text.toString()
+
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
 
         if(email == MASTER_EMAIL && password == MASTER_PASSWORD) {
             val message = "$email logado.\nSeja bem vindo!"
@@ -30,6 +56,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             startActivity(intent)
+        }
+
+        else {
+            loginErrorMessageText.text = LOGIN_ERROR_MESSAGE
         }
 
 
