67be410fdd7f04d8eca613e39caddedbc96b7d8b
Mayara Fernandes <mayara.mpf21@gmail.com>
2019-08-16 14:07:16 -0300

    Simple e-mail and password validation

--- app/src/main/java/com/mayarafernandes/movieplayer/MainActivity.kt
+++ app/src/main/java/com/mayarafernandes/movieplayer/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.EditText
 
 const val EXTRA_MESSAGE = "com.mayarafernandes.movieplayer.MESSAGE"
+const val MASTER_EMAIL = "admin@mail.com"
+const val MASTER_PASSWORD = "admin123"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,15 +19,19 @@ class MainActivity : AppCompatActivity() {
 
     fun onLogin(view: View) {
 
-        val emailText = findViewById<EditText>(R.id.editText3)
-        val email = emailText.text.toString()
-        val message = "$email logado!"
-        
-        val intent = Intent(this, DisplayMessage::class.java). apply {
-            putExtra(EXTRA_MESSAGE, message)
+        val email = findViewById<EditText>(R.id.emailEditText).text.toString()
+        val password = findViewById<EditText>(R.id.passwordEditText).text.toString()
+
+        if(email == MASTER_EMAIL && password == MASTER_PASSWORD) {
+            val message = "$email logado.\nSeja bem vindo!"
+
+            val intent = Intent(this, DisplayMessage::class.java). apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
+
+            startActivity(intent)
         }
 
-        startActivity(intent)
 
     }
 
